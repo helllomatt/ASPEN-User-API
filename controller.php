55e52e53e22f3cbf66ffd7b59f6a4ed2a82eb3bc
matt <me@helllomatt.com>
2017-02-13 16:44:48 -0600

    new paths, removal of old paths

--- controller.php
+++ controller.php
@@ -6,13 +6,13 @@ use ASPEN\Response;
 $app = new ASPEN\App('OAuth2');
 $app->version(1);
 
-$app->get('oauth2/token/', function() {
-    $auth = new Users\Auth();
+$app->get('users/authenticate/', function() {
+    $auth = new Users\OAuth2();
     $auth->handleTokenRequest();
 });
 
-$app->get('oauth2/validate/', function() {
-    $auth = new Users\Auth();
+$app->get('users/validate-authentication/', function() {
+    $auth = new Users\OAuth2();
     $auth->validate();
 
     $response = new Response();
@@ -21,15 +21,6 @@ $app->get('oauth2/validate/', function() {
     } else {
         $response->error('Unauthorized.');
     }
-});
-
-$app->get('identity/', function() {
-    $auth = new Users\Auth();
-    $auth->requireValidToken();
-
-    $response = new Response();
-    $response->add('identity', $auth->getUser());
-    $response->success();
 });
 
 return $app;
